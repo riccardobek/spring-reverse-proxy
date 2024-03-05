1987b0a4516bd4fdc3875fe40bfd701a63dd4762
Riccardo Bernucci <37105282+riccardobek@users.noreply.github.com>
2024-03-05 22:57:17 +0100

    resolved problem with websocket
    The post request for enabling websocket were refused because not well configured the restTemplate

--- spring-cloud-starter-gateway-mvc/src/main/java/it/ber/reverseproxy/demo/springcloudstartergateway/mvc/grafana/GrafanaController.java
+++ spring-cloud-starter-gateway-mvc/src/main/java/it/ber/reverseproxy/demo/springcloudstartergateway/mvc/grafana/GrafanaController.java
@@ -1,6 +1,11 @@
 package it.ber.reverseproxy.demo.springcloudstartergateway.mvc.grafana;
 
 import it.ber.reverseproxy.demo.springcloudstartergateway.mvc.grafana.exception.GrafanaProxyException;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +13,30 @@ import org.springframework.cloud.gateway.mvc.ProxyExchange;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 
-@Controller
+@RestController
 public class GrafanaController {
 
     @Autowired
     private GrafanaService grafanaService;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private RestTemplate restTemplate;
 
 
     /**
@@ -55,14 +66,14 @@ public class GrafanaController {
         //Estrapolo percorso
         String path = proxy.path("/grafana");
 
-
         try {
 
             URI uriBuilder = new URI(grafanaService.getUrlGrafana() + path);
 
             if (RequestMethod.valueOf(httpServletRequest.getMethod()).equals(RequestMethod.POST)) {
                 //Le chiamate POST non funzionano tramite proxy
-                RestTemplate template = new RestTemplate();
+                RestTemplate template = restTemplate();
+
 
                 HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
                 return template.postForObject(uriBuilder.toString(), requestEntity, ResponseEntity.class);
@@ -75,6 +86,28 @@ public class GrafanaController {
             return null;
         }
 
+    }
+
+    private RestTemplate restTemplate()
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+
+        if(restTemplate == null) {
+            TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
+            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy).build();
+
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+            requestFactory.setHttpClient(httpClient);
+
+            restTemplate = new RestTemplate(requestFactory);
+        }
+
+        return restTemplate;
     }
 
 
